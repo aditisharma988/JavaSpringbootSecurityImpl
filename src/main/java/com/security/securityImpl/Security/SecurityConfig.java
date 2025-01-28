40ef9ebb2de7e471ceed6d2e29a71bb692eb172f
aditisharma988 <aditisharma140802@gmail.com>
2025-01-28 11:45:23 +0530

    Created bean of AutheticationProvider to interact with db and set basic passwordEncoder, userDetailsSErvice there and removed bean of userDetailsService with default values.

--- src/main/java/com/security/securityImpl/Security/SecurityConfig.java
+++ src/main/java/com/security/securityImpl/Security/SecurityConfig.java
@@ -1,21 +1,28 @@
 package com.security.securityImpl.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -40,30 +47,38 @@ public class SecurityConfig {
     }
 
 
-        @Bean
-        public UserDetailsService userDetailsService(){
+    //to customize authetication using database
+    @Bean
+    AuthenticationProvider authenticationProvider(){
+
+        //for db
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        return daoAuthenticationProvider;
+
+    }
 
 
-        //first user
-            UserDetails user1 = User
-                    .withDefaultPasswordEncoder()
-                    .username("aditii")
-                    .password("Adi@123")
-                    .roles("USER")
-                    .build();
-
-            //second user
-            UserDetails user2 = User
-                    .withDefaultPasswordEncoder()
-                    .username("mahi")
-                    .password("Mahi@123")
-                    .roles("USER")
-                    .build();
-
-
-
-
-        return  new InMemoryUserDetailsManager(user1 , user2);
-        }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        //first user
+//        UserDetails user1 = User
+//                .withDefaultPasswordEncoder()
+//                .username("aditii")
+//                .password("Adi@123")
+//                .roles("USER")
+//                .build();
+//
+//        //second user
+//        UserDetails user2 = User
+//                .withDefaultPasswordEncoder()
+//                .username("mahi")
+//                .password("Mahi@123")
+//                .roles("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(user1, user2);
+//    }
 
 }
