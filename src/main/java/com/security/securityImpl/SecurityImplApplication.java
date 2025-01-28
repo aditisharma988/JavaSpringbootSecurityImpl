454b1b4d19c3f519a65117105e12d545429a2f8e
aditisharma988 <aditisharma140802@gmail.com>
2025-01-28 17:12:01 +0530

    Basic dependencies and plugins added for databse connectivity and also databse related set up also in app.prop file

--- src/main/java/com/security/securityImpl/SecurityImplApplication.java
+++ src/main/java/com/security/securityImpl/SecurityImplApplication.java
@@ -3,8 +3,10 @@ package com.security.securityImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@ComponentScan(basePackages = "com.security.securityImpl")
 public class SecurityImplApplication {
 
 	public static void main(String[] args) {
