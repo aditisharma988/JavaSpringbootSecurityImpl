e0aba00b5f28c19063a4c3c7f0517fcd58e39039
aditisharma988 <aditisharma140802@gmail.com>
2025-01-28 18:10:24 +0530

    Added BcryptEncoder to encode password and save in db in hashed form

--- src/main/java/com/security/securityImpl/Service/UserService.java
+++ src/main/java/com/security/securityImpl/Service/UserService.java
@@ -3,6 +3,7 @@ package com.security.securityImpl.Service;
 import com.security.securityImpl.Model.Users;
 import com.security.securityImpl.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +13,12 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
 
     public Users register(Users users){
+
+        users.setPassword(bCryptPasswordEncoder.encode(users.getPassword()));
          return userRepository.save(users);
 
     }
