d2b4f185ebfa671605344e07f363e72956f970da
aditisharma988 <aditisharma140802@gmail.com>
2025-01-30 16:49:41 +0530

    Verification of jwt token and basic login, register api endpoint with generation of accessToken

--- src/main/java/com/security/securityImpl/Security/SecurityConfig.java
+++ src/main/java/com/security/securityImpl/Security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.security.securityImpl.Security;
 
 import com.security.securityImpl.Service.JWTService;
+import com.security.securityImpl.config.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +25,9 @@ public class SecurityConfig {
     @Autowired
     private UserDetailsService userDetailsService;
 
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -45,6 +50,7 @@ public class SecurityConfig {
 
                 //to make application stateless
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 
                 .build();
     }
--- src/main/java/com/security/securityImpl/Service/JWTService.java
+++ src/main/java/com/security/securityImpl/Service/JWTService.java
@@ -1,8 +1,10 @@
 package com.security.securityImpl.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
@@ -13,6 +15,7 @@ import java.sql.Date;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JWTService {
@@ -44,4 +47,49 @@ public class JWTService {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(secretKey.getBytes());
     }
+
+    public String extractUsername(String token) {
+
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims,T> claimResolver){
+        final Claims claims= extractAllClaims(token);
+        return claimResolver.apply(claims);
+
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getkey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+//    private Claims extractAllClaims(String token) {
+//        return  Jwts.parser()
+//                .verifyWith(getkey())
+//                .build()
+//                .parseSignedClaims(token)
+//                .getBody();
+//    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String userName = extractUsername(token);
+        return(userDetails.equals(userDetails.getUsername())&& !isTokenExpired(token));
+
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new java.util.Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return (Date) extractClaim(token,Claims::getExpiration);
+    }
+
+
+
+
 }
